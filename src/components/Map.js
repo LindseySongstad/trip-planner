98607f19c695336d240f404ac2526b4c1b66613c
LindseySongstad <lindsey.songstad@outlook.com>
2021-06-24 10:12:33 -0700

    Change map library
    trying react-google-maps/api instead of google-map-react

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,45 +1,30 @@
-import { useState } from 'react'
-import GoogleMapReact from 'google-map-react'
-import MapMarker from './MapMarker'
-import InfoBox from './InfoBox'
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
-const Map = ({ center, zoom, places }) => {
-    const [selected, setSelected] = useState(null)
-    const markers = places.map((place) => {
-        if (place.lat) {
-            return <MapMarker key={place.id} lat={place.lat} lng={place.lng} onClick={() => setSelected({ name: place.name })} />
-        }
-        return null
-    })
+const center = {
+  lat: 30,
+  lng: -100
+};
 
-    return (
-        <div className='map-container'>
-            <div className='map'>
+function Map() {
+  return (
+    <div className='map-container'>
+      <LoadScript
+        googleMapsApiKey=''
 
-            {console.log('hello')}
-
-                <GoogleMapReact
-
-                    bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY}}
-                    defaultCenter={center}
-                    defaultZoom={zoom}
-                >
-                    {markers}
-                    
-                </GoogleMapReact>
-                {selected && <InfoBox info={selected} />}
-            </div>
-        </div>
-    )
+      >
+        <GoogleMap
+          mapContainerClassName='map'
+          center={center}
+          zoom={4.5}
+        >
+          { /* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  )
 }
 
-Map.defaultProps = {
-    center: {
-        lat: 40.0,
-        lng: -100.0
-    },
-    zoom: 4.5
-}
-
-export default Map;
+export default React.memo(Map)
